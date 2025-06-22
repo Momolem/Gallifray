28922b0ce421aef8fd12784f19738af345c68a7a
Moritz Vogel <contact@vogel.business>
2025-06-22 21:17:31 +0200

    Minor changes and automatic extension activation

---
+++ files/scripts/enable-gnome-extensions.sh
@@ -0,0 +1,15 @@
+gnome-extensions enable HeadsetControl@lauinger-clan.de
+gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
+gnome-extensions enable apps-menu@gnome-shell-extensions.gcampax.github.com
+gnome-extensions enable caffeine@patapon.info
+gnome-extensions enable clipboard-history@alexsaveau.dev
+gnome-extensions enable compiz-alike-magic-lamp-effect@hermes83.github.com
+gnome-extensions enable dash-to-panel@jderose9.github.com
+gnome-extensions enable launch-new-instance@gnome-shell-extensions.gcampax.github.com
+gnome-extensions enable no-overview@fthx
+gnome-extensions enable places-menu@gnome-shell-extensions.gcampax.github.com
+gnome-extensions enable smart-home@chlumskyvaclav.gmail.com
+gnome-extensions enable trayIconsReloaded@selfmade.pl
+gnome-extensions enable window-list@gnome-shell-extensions.gcampax.github.com
+
+gnome-extensions disable background-logo@fedorahosted.org
